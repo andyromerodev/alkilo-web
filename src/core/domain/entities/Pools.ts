84d93b19d5a221571cc2dc970a04b3809f96426b
andyromero <andy910304@gmail.com>
2024-12-14 13:06:47 -0500

    Update Pools.ts

---
+++ src/core/domain/entities/Pools.ts
@@ -0,0 +1,10 @@
+export interface Pools {
+    id: number;
+    title: string;
+    description: string;
+    price_per_night: number;
+    bedrooms: number;
+    bathrooms: number;
+    is_available: boolean;
+    city: string;
+  }
